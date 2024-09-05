3c4b306fe839c77e6a9263caf2f9574c352ecaef
Daniel <danoladesign@gmail.com>
2024-09-05 14:45:06 -0400

    RWN030: fixed sitemap error

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -4,26 +4,26 @@ import { getArticles } from "@/lib/articles";
 export const revalidate = 3600;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const projects = await getArticles();
+  const { articles } = await getArticles(); // Destructure the articles array from the returned object
 
   const baseUrls: MetadataRoute.Sitemap = [
     {
-      url: "https://www.realworldnews.org/",
+      url: "https://distort-apps.vercel.app",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://www.realworldnews.org/resources",
+      url: "https://distort-apps.vercel.app/contact",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
   ];
 
-  const projectUrls: MetadataRoute.Sitemap = projects.map((project) => ({
-    url: `https://distort-apps.vercel.app/articles/${project.slug}`,
-    lastModified: new Date(project.date),
+  const projectUrls: MetadataRoute.Sitemap = articles.map((article) => ({
+    url: `https://distort-apps.vercel.app/articles/${article.slug}`,
+    lastModified: new Date(article.date),
     changeFrequency: "weekly" as const,  
     priority: 0.7,
   }));
